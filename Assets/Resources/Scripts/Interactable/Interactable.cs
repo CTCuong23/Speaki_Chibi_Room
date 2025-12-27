f13be3669e338d73428154c5b689469f373f81a8
CTCuong23 <phucuong10082003@gmail.com>
2025-12-27 19:18:29 +0700

    Add expression

--- Assets/Resources/Scripts/Interactable/Interactable.cs
+++ Assets/Resources/Scripts/Interactable/Interactable.cs
@@ -7,6 +7,9 @@ public class Interactable : MonoBehaviour
     private AudioSource audioSource;
     private bool canInteract = false;
 
+    [Header("Cài đặt hình ảnh")]
+    [SerializeField] GameObject emotePrefab; // Kéo Prefab trái tim vào đây
+    [SerializeField] Transform spawnPoint;   // Điểm xuất hiện (thường là trên đầu Speaki)
     void Start()
     {
         hintText.SetActive(false);
@@ -31,6 +34,18 @@ public class Interactable : MonoBehaviour
         }
 
         Debug.Log("Speaki says: Cuayo!");
+
+        if (emotePrefab != null)
+        {
+            // Lấy vị trí trên đầu bé Speaki (thông qua tag Player)
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            Vector3 spawnPos = player.transform.position + new Vector3(0, 1.5f, 0); // Cao hơn đầu 1.5 đơn vị
+
+            // Tạo ra trái tim
+            Instantiate(emotePrefab, spawnPos, Quaternion.identity);
+        }
+
+        Debug.Log("Cuayo with Love!");
     }
 
     private void OnTriggerEnter2D(Collider2D other)
